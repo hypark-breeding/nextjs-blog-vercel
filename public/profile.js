6c2e99e366e8e3f03a34b0d9980174a013aced5a
hypark-breeding <hypark.temp@gmail.com>
2021-04-18 01:38:05 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ public/profile.js
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+
+const YourComponent = () => (
+    <Image  
+        src="/images/logo.PNG"
+        height={144}
+        width={144}
+        alt = "jumsim"
+    />
+)

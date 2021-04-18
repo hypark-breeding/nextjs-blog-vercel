56bd5e7567b0b6f82b2282740440c603de15e848
hypark-breeding <82245470+hypark-breeding@users.noreply.github.com>
2021-04-18 11:35:26 +0900

    Update layout.js

--- components/layout.js
+++ components/layout.js
@@ -19,8 +19,8 @@ export default function Layout({ children, home }) {
         <meta
           property="og:image"
           content={`https://og-image.vercel.app/${encodeURI(
-            siteTitle
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+            "https://api.breeeeeding.com/images/trainer/N-1-20210318163544.png"
+          )}`}
         />
         <meta name="og:title" content={siteTitle} />
         <meta name="twitter:card" content="summary_large_image" />
